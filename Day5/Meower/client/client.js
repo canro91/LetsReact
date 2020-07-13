c80c982e957ed7c1d6228acb375251b09135d299
canro91 <canro91@hotmail.com>
2020-07-12 22:13:57 -0500

    Create and list meows

--- Day5/Meower/client/client.js
+++ Day5/Meower/client/client.js
@@ -1,10 +1,10 @@
 const API_URL = 'http://192.168.99.100:3000/meows';
 
 const loading = document.querySelector('.loading');
-loading.style.display = 'none';
+loading.style.display = '';
 
 const form = document.querySelector('form');
-form.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
     const name = formData.get('name');
@@ -24,5 +24,50 @@ form.addEventListener('submit', (event)=>{
         headers: {
             'content-type': 'application/json'
         }
+    })
+    .then(response => response.json())
+    .then(createdMeow => {
+        form.reset();
+
+        loading.style.display = 'none';
+        form.style.display = '';
+
+        listAllMeows();
     });
 });
+
+const meowsElement = document.querySelector('.meows');
+function listAllMeows(){
+    meowsElement.innerHTML = '';
+
+    fetch(API_URL, {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(meows => {
+
+        meows.forEach(meow =>{
+            const div = document.createElement('div');
+            const header = document.createElement('h3');
+            header.textContent = meow.name;
+            
+            const content = document.createElement('p');
+            content.textContent = meow.content;
+
+            const date = document.createElement('small');
+            date.textContent = new Date(meow.created);
+            
+            div.appendChild(header);
+            div.appendChild(content);
+            div.appendChild(date);
+            meowsElement.appendChild(div);
+            
+        });
+
+        loading.style.display = 'none';
+    });
+}
+listAllMeows();
--- Day5/Meower/server/app.js
+++ Day5/Meower/server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const monk = require('monk');
 
-const db = monk('mongo:21017/meows')
-
 const app = express();
+
+const db = monk('mongo/meower');
+const meows = db.get('meows');
+
 app.use(cors());
 app.use(express.json());
 
@@ -14,14 +16,29 @@ app.get('/', (request, response) => {
     });
 });
 
+app.get('/meows', (req, res) => {
+    meows
+    .find({}, { sort: { created: -1 } })
+    .then(meows => {
+        res.json(meows);
+    });
+});
+
 app.post('/meows', (req, res) => {
     if (isValidMeow(req.body)) {
         const meow = {
             name: req.body.name.toString(),
-            content: req.body.content.toString()
-        }
+            content: req.body.content.toString(),
+            created: new Date()
+        };
 
-        console.log(meow);
+        meows
+            .insert(meow)
+            .then(createdMeow => {
+                console.log(createdMeow);
+                res.json(createdMeow);
+            })
+            .catch(err => {console.log(err);});
     } else {
         res.status(422);
         res.json({
@@ -30,9 +47,9 @@ app.post('/meows', (req, res) => {
     }
 });
 
-function isValidMeow(meow){
+function isValidMeow(meow) {
     return meow.name && meow.name.toString().trim() !== ''
-            && meow.content && meow.content.toString().trim() !== '';
+        && meow.content && meow.content.toString().trim() !== '';
 }
 
 app.listen(3000, () => {
