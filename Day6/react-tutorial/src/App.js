a3e9d5fcb89a9759da68845b5db501a341035c22
canro91 <canro91@hotmail.com>
2020-07-14 12:28:45 -0500

    Crate Table component

--- Day6/react-tutorial/src/App.js
+++ Day6/react-tutorial/src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import Table from './Table'
 
 class App extends Component {
     render(){
         return (
-            <div className="App">
-                <h1>Hello, world!!!</h1>
+            <div className="container">
+                <Table />
             </div>
         )
     }
---
+++ Day6/react-tutorial/src/Table.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Charlie</td>
+                <td>Janitor</td>
+            </tr>
+            <tr>
+                <td>Mac</td>
+                <td>Bouncer</td>
+            </tr>
+            <tr>
+                <td>Dee</td>
+                <td>Aspiring actress</td>
+            </tr>
+            <tr>
+                <td>Dennis</td>
+                <td>Bartender</td>
+            </tr>
+        </tbody>
+    )
+}
+
+class Table extends Component {
+    render() {
+        return (
+            <table>
+                <TableHeader />
+                <TableBody />
+            </table>
+        )
+    }
+}
+
+export default Table
