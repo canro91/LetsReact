06b77621ee88966fbc3a03ef10f3d7b7e0a5325d
canro91 <canro91@hotmail.com>
2020-07-30 00:19:16 -0500

    Add links

--- Day11/bookmarks/src/App.js
+++ Day11/bookmarks/src/App.js
@@ -1,23 +1,42 @@
 import React, { Fragment } from 'react';
 import './index.css'
 
-const App = () => {
-    const linkImageStyle = {
+const LinkCard = ({ name, href }) => {
+    return (
+        <div className="linkCard">
+            <div className="linkCardImage" />
+            <div className="linkCardLink">
+                <h2><a href={href}>{name}</a></h2>
+            </div>
+        </div>
+    )
+}
 
-    };
+const App = () => {
+    const [cards, setCards] = React.useState([]);
+    const [newCard, setNewCard] = React.useState({ linkName: '', linkHref: '' });
+
+    const onAddCardClick = (newCard) => {
+        let oldCards = cards;
+        let newCards = [...oldCards, newCard];
+        setCards(newCards);
+        setNewCard({ linkName: '', linkHref: '' });
+    }
 
     return (
         <Fragment>
             <nav className="navigation">
                 <main>
                     <div className="leftContent">
-                        <form>
+                        <form onClick={e => e.preventDefault()}>
                             <h2 className="formTitle">Add a bookmark</h2>
                             <div>
                                 <label
                                     htmlFor="linkTitle"
                                     className="formLabel">Your bookmark title</label>
                                 <input
+                                    value={newCard.linkName}
+                                    onChange={e => setNewCard({ ...newCard, linkName: e.currentTarget.value })}
                                     type="text"
                                     name="linkTitle"
                                     minLength="1"
@@ -29,22 +48,23 @@ const App = () => {
                                     htmlFor="linkUrl"
                                     className="formLabel">Your bookmark url</label>
                                 <input
+                                    value={newCard.linkHref}
+                                    onChange={e => setNewCard({ ...newCard, linkHref: e.currentTarget.value })}
                                     type="text"
                                     name="linkUrl"
                                     minLength="7"
                                     placeholder="https://example.com" />
                             </div>
-                            <button>Add</button>
+                            <button onClick={() => onAddCardClick(newCard)}>Add</button>
                         </form>
 
                     </div>
                     <div className="rightContent">
-                        <div className="linkCard">
-                            <div className="linkCardImage" />
-                            <div className="linkCardLink">
-                                <h2><a href="#">My Link</a></h2>
-                            </div>
-                        </div>
+                        <Fragment>
+                            {cards.map((card, i) => (
+                                <LinkCard key={i} name={card.linkName} href={card.linkHref} />
+                            ))}
+                        </Fragment>
                     </div>
                 </main>
             </nav>
