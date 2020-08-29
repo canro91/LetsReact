797cc712c29319adbf78ce6297e8951c67328763
canro91 <canro91@hotmail.com>
2020-08-29 00:36:41 -0500

    Load Weather component

--- Day16/weather-app/src/App.js
+++ Day16/weather-app/src/App.js
@@ -2,10 +2,44 @@ import React from 'react';
 import './App.css';
 import Weather from './components/Weather';
 
+// Yeah, I know. Don't store sensitive keys. For testing purproses only.
+const API_KEY = '23b7f9c35607924f30a9ada97b7faa95';
+
 function App() {
+  const [forecast, setForecast] = React.useState({});
+
+  React.useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Medellin&appid=${API_KEY}&units=metric`)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      const temperature = {
+        city: response.name,
+        country: response.sys.country,
+
+        current: response.main.temp,
+        min: response.main.temp_min,
+        max: response.main.temp_max,
+
+        description: response.weather[0].description,
+        iconId: response.weather[0].id
+      };
+      setForecast(temperature);
+    })
+    .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
-      <Weather />
+      <Weather
+        city={forecast.city}
+        country={forecast.country}
+        current={forecast.current}
+        min={forecast.min}
+        max={forecast.max}
+        description={forecast.description}
+        iconId={forecast.iconId}
+      />
     </div>
   );
 }
--- Day16/weather-app/src/components/Weather.js
+++ Day16/weather-app/src/components/Weather.js
@@ -1,19 +1,55 @@
 import React from 'react';
 
-const Weather = (props) => {
+const Weather = ({ city, country, current, min, max, description, iconId }) => {
+    const icons = {
+        thunderstorm: 'wi-thunderstorm',
+        drizzle: 'wi-sleet',
+        rain: 'wi-storm-showers',
+        snow: 'wi-snow',
+        atmosphere: 'wi-fog',
+        clear: 'wi-day-sunny',
+        clouds: 'wi-day-fog'
+    };
+
+    const mapIconToStyle = (iconId) => {
+        switch (true) {
+            case iconId >= 200 && iconId <= 232:
+                return icons.thunderstorm;
+
+            case iconId >= 300 && iconId <= 321:
+                return icons.drizzle;
+
+            case iconId >= 500 && iconId <= 531:
+                return icons.rain;
+
+            case iconId >= 600 && iconId <= 622:
+                return icons.snow;
+
+            case iconId >= 700 && iconId <= 781:
+                return icons.atmosphere;
+
+            case iconId == 800:
+                return icons.clear;
+
+            case iconId >= 800 && iconId <= 804:
+                return icons.clouds;
+        }
+
+    };
+
     return (
         <div className="container">
             <div className="cards">
-                <h1>Medellin</h1>
+                <h1>{city}, {country}</h1>
                 <h5 className="py-4">
-                    <i className="wi wi-day-sunny display-1"></i>
+                    <i className={`wi ${mapIconToStyle(iconId)} display-1`}></i>
                 </h5>
-                <h2 className="py-2">25&deg;</h2>
+                <h2 className="py-2">{current}&deg;</h2>
                 <h3>
-                    <span className="px-4">20&deg;</span>
-                    <span className="px-4">20&deg;</span>
+                    <span className="px-4">{min}&deg;</span>
+                    <span className="px-4">{max}&deg;</span>
                 </h3>
-                <h4 className="py-3">Slow Rain</h4>
+                <h4 className="py-3">{description}</h4>
             </div>
 
         </div>
