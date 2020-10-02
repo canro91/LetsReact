e64f27ef6887bd495b22c326aa4fce2c2d3b622b
canro91 <canro91@hotmail.com>
2020-10-02 18:13:02 -0500

    Draw snake head and snack

--- Day18/react-snake/src/App.js
+++ Day18/react-snake/src/App.js
@@ -7,37 +7,62 @@ for (let index = 0; index < GRID_SIZE; index++) {
   GRID.push(index);
 }
 
-const isBorder = (x, y) =>
+const isBorderCell = (x, y) =>
   x === 0 || y === 0 || x === GRID_SIZE - 1 || y === GRID_SIZE - 1;
 
-const getClassNames = (x, y) => {
-  const border = isBorder(x, y) ? 'grid-cell-border' : '';
-  return `grid-cell ${border}`;
+const areAtTheSamePosition = (x, y, x1, y1) =>
+  x === x1 && y === y1;
+
+const getClassNames = (x, y, snake, snack) => {
+  const border = isBorderCell(x, y) ? 'grid-cell-border' : '';
+  const snakeCell = areAtTheSamePosition(x, y, snake.position.x, snake.position.y) ? 'grid-cell-snake': '';
+  const snackCell = areAtTheSamePosition(x, y, snack.position.x, snack.position.y) ? 'grid-cell-snack' : '';
+  return `grid-cell ${border} ${snakeCell} ${snackCell}`;
 };
 
 function App() {
+  const [snake, setSnake] = React.useState({ position: { x: 10, y: 10 } });
+  const [snack, setSnack] = React.useState({ position: { x: 17, y: 17 } });
+
   return (
     <div className="app">
       <h1>Snake</h1>
-      <Grid />
+      <Grid snake={snake} snack={snack} />
     </div>
   );
 }
 
-const Grid = () => (
+const Grid = ({ snake, snack }) => (
   <div>
-    { GRID.map(y => <Row y={y} key={y} />) }
+    {
+      GRID.map(y =>
+        <Row
+          y={y}
+          snake={snake}
+          snack={snack}
+          key={y}
+      />)
+    }
   </div>
 );
 
-const Row = ({ y }) => (
+const Row = ({ y, snake, snack }) => (
   <div className="grid-row">
-    { GRID.map(x => <Cell y={y} x={x} key={x} />) }
+    {
+      GRID.map(x =>
+        <Cell
+          y={y}
+          x={x}
+          snake={snake}
+          snack={snack}
+          key={x}
+        />)
+    }
   </div>
 );
 
-const Cell = ({ y, x }) => (
-  <div className={getClassNames(x, y)} />
+const Cell = ({ y, x, snake, snack }) => (
+  <div className={getClassNames(x, y, snake, snack)} />
 );
 
 export default App;
