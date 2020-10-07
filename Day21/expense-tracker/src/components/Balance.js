f92515576ed4c8167fa36d35825ab988d61d6cef
canro91 <canro91@hotmail.com>
2020-10-07 12:26:53 -0500

    Calculate total from context

--- Day21/expense-tracker/src/components/Balance.js
+++ Day21/expense-tracker/src/components/Balance.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Balance = () => {
+    const {transactions} = React.useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
     return (
         <>
             <h4>Your Balance</h4>
-            <h1 id="balance">$0.00</h1>
+            <h1 id="balance">${total}</h1>
         </>
     );
 }
