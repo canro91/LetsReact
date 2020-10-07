e824acbe8cfdb1e2db657f536819db5667d7fa48
canro91 <canro91@hotmail.com>
2020-10-07 12:25:21 -0500

    Calculate income and expenses from context

--- Day21/expense-tracker/src/components/Summary.js
+++ Day21/expense-tracker/src/components/Summary.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Summary = () => {
+    const { transactions } = React.useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1
+    ).toFixed(2);
+
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
+                <p className="money plus">+${income}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p className="money minus">-$0.00</p>
+                <p className="money minus">-${expense}</p>
             </div>
         </div>
     );
