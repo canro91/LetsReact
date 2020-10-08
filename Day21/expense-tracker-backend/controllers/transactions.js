e3683660744c3b89341de1d2a07b323da31f15e9
canro91 <canro91@hotmail.com>
2020-10-08 17:57:18 -0500

    Add, remove and list transactions

--- Day21/expense-tracker-backend/controllers/transactions.js
+++ Day21/expense-tracker-backend/controllers/transactions.js
@@ -1,14 +1,73 @@
+const Transaction = require('../models/Transaction');
+
 // GET /api/v1/transactions
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const trasactions = await Transaction.find();
+
+        return res.status(200).json({
+            success: true,
+            count: trasactions.length,
+            data: trasactions
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            data: 'Server error'
+        });
+    }
 };
 
 // POST /api/v1/transactions
-exports.addTransaction = (req, res, next) => {
-    res.send('POST transactions');
+exports.addTransaction = async (req, res, next) => {
+    try {
+        const transaction = await Transaction.create(req.body);
+        return res.status(201).json({
+            success: true,
+            data: transaction
+        });
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            const messages = Object.values(err.errors).map(val => val.message);
+
+            return res.status(400).json({
+                success: false,
+                error: messages
+            });
+
+        } else {
+            return res.status(500).json({
+                success: false,
+                data: 'Server error'
+            });
+        }
+    }
 };
 
 // DELETE /api/v1/transactions/:id
-exports.deleteTransaction = (req, res, next) => {
-    res.send('DELETE transaction');
+exports.deleteTransaction = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const transaction = await Transaction.findById(id)
+
+        if (!transaction) {
+            return res.status(404).json({
+                success: false,
+                error: 'No transaction found'
+            });
+        }
+
+        await transaction.remove();
+
+        return res.status(200).json({
+            success: true,
+            data: transaction
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            data: 'Server error'
+        });
+    }
 };
--- Day21/expense-tracker-backend/server.js
+++ Day21/expense-tracker-backend/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env'});
+
+connectDB();
 
 const transactions = require('./routes/transaction');
 
 const app = express();
+app.use(express.json());
 app.use('/api/v1/transactions', transactions);
 
 const port = process.env.PORT || 5000;
