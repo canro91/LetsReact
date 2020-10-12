9a6d508f5c7729e0a216b3ceeaa669b2d346ff76
canro91 <canro91@hotmail.com>
2020-10-12 12:28:57 -0500

    Create a store and a reducer

--- Day24/README.md
+++ Day24/README.md
@@ -1,3 +1,5 @@
 # Notes
 
+* Reducer function is called when creating the store. A reducer should define the initial state too.
+* An action is an object with a property `type`
 * [npm install --save](https://stackoverflow.com/questions/19578796/what-is-the-save-option-for-npm-install)
--- Day24/redux-tutorial/src/index.js
+++ Day24/redux-tutorial/src/index.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
+
+// Step 2. Create a reducer
+// It's just like the function you use with React.useReducer
+// A reducer should define the initial state too
+const initialState = {
+  count: 0
+};
+
+const reducer = (state = initialState, action) => {
+  console.log('2. I am the reducer', state, action);
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+
+    case 'RESET':
+      return { count: 0 };
+
+    default:
+      return state;
+  }
+}
+
+// Step 1. Create a store
+// Redux call the reducer when creating the store
+// Notice the console.log order
+console.log('1. I am about to create the store')
+const store = createStore(reducer);
+console.log('3. It is after creating the store');
+
+// Simulate some dispatch calls
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+store.dispatch({ type: 'RESET' });
 
 ReactDOM.render(
   <React.StrictMode>
