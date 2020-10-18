d223a1e9558a041d3c30cd28a8a521650ecb784c
canro91 <canro91@hotmail.com>
2020-10-18 14:31:05 -0500

    Move store to its own folder

--- Day27/movies-app/src/App.js
+++ Day27/movies-app/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
@@ -8,11 +6,11 @@ import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
+    <>
       <Navbar />
       <Home />
       <Footer />
-    </Provider>
+    </>
   );
 }
 
--- Day27/movies-app/src/store/index.js
+++ Day27/movies-app/src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers/index';
+import rootReducer from '../reducers';
 import { createLogger } from 'redux-logger';
 import thunk  from 'redux-thunk';
 
