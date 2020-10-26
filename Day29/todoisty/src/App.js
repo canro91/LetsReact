aae642e10f97a1fbe92420131cd12260e40f2fbe
canro91 <canro91@hotmail.com>
2020-10-25 21:07:24 -0500

    Add dark mode

--- Day29/todoisty/src/App.js
+++ Day29/todoisty/src/App.js
@@ -1,15 +1,21 @@
+import React from 'react';
 import Content from './components/layout/Content';
 import Header from './components/layout/Header';
 import { ProjectsProvider, SelectedProjectProvider } from './context';
 
-function App() {
+function App({ darkModeDefault = false }) {
+  const [darkMode, setDarkMode] = React.useState(darkModeDefault);
+
   return (
     <SelectedProjectProvider>
       <ProjectsProvider>
-        <div className="App">
-          <Header />
+        <main
+          data-testid="application"
+          className={darkMode ? 'darkmode' : undefined}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
           <Content />
-        </div>
+        </main>
       </ProjectsProvider>
     </SelectedProjectProvider>
   );
--- Day29/todoisty/src/components/layout/Header.js
+++ Day29/todoisty/src/components/layout/Header.js
@@ -1,20 +1,51 @@
 import React from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
+import AddTask from '../AddTask';
 
-const Header = () => {
+const Header = ({ darkMode, setDarkMode }) => {
+    const [shouldShowMain, setShouldShowMain] = React.useState(false);
+    const [showQuickAddTask, setShowQuickAddTask] = React.useState(false);
+
     return (
         <header className="header" data-testid="header">
             <nav>
                 <div className="logo">
-                    <img src="/images/logo.png" alt="Todoisty"/>
+                    <img src="/images/logo.png" alt="Todoist" />
                 </div>
                 <div className="settings">
                     <ul>
-                        <li className="settings__add" data-testid="quick-add-task-action">+</li>
-                        <li className="settings__darkmode" data-testid="dark-mode-action"><FaPizzaSlice /></li>
+                        <li className="settings__add">
+                            <button
+                                data-testid="quick-add-task-action"
+                                type="button"
+                                onClick={() => {
+                                    setShowQuickAddTask(true);
+                                    setShouldShowMain(true);
+                                }}
+                            >
+                                +
+                            </button>
+                        </li>
+                        <li className="settings__darkmode">
+                            <button
+                                data-testid="dark-mode-action"
+                                aria-label="Darkmode on/off"
+                                type="button"
+                                onClick={() => setDarkMode(!darkMode)}
+                            >
+                                <FaPizzaSlice />
+                            </button>
+                        </li>
                     </ul>
                 </div>
             </nav>
+
+            <AddTask
+                showAddTaskMain={false}
+                shouldShowMain={shouldShowMain}
+                showQuickAddTask={showQuickAddTask}
+                setShowQuickAddTask={setShowQuickAddTask}
+            />
         </header>
     );
 }
